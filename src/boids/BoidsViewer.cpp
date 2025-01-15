d887e2eb247d08f929ebc5bcf06aa0e275239889
Tom D'export <71851742+tomdexp@users.noreply.github.com>
2025-01-15 13:58:20 +0100

    Move MyViewer to its own cpp file

---
+++ src/boids/BoidsViewer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ELE64f86f1550ed8 on 15/01/2025.
+//
+
+#include "BoidsViewer.h"
---
+++ src/boids/BoidsViewer.h
@@ -0,0 +1,16 @@
+//
+// Created by ELE64f86f1550ed8 on 15/01/2025.
+//
+
+#ifndef BOIDSVIEWER_H
+#define BOIDSVIEWER_H
+
+
+
+class BoidsViewer {
+
+};
+
+
+
+#endif //BOIDSVIEWER_H
